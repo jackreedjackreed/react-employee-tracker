093849d2124af5c7610ae4b3f66b56c3673c3846
J-Boogie <jackr353@gmail.com>
2021-03-24 16:01:20 -0500

    set up install and config of tailwind

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Directory from "./components/Directory";
-
+import './tailwind.output.css';
 
 function App() {
   return (
