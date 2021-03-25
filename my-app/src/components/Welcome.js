c0ec363065e45c8aba1efba8258a244f447912a5
J-Boogie <jackr353@gmail.com>
2021-03-25 14:45:41 -0500

    paste in some tailwind code and get new chat message to pop

--- my-app/src/components/Welcome.js
+++ my-app/src/components/Welcome.js
@@ -20,7 +20,14 @@ class Welcome extends Component {
         render() {
             return (
                 <div>
-                    BasicComponent
+                    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+                    <div class="flex-shrink-0">
+            </div>
+            <div>
+     <div class="text-xl font-medium text-black">ChitChat</div>
+        <p class="text-blue-500">You have a new message!</p>
+    </div>
+            </div>
                 </div>
             )
         }        
