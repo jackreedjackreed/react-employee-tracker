d38ea0562a896be27eb49e11d5c74eba9dea4c67
J-Boogie <jackr353@gmail.com>
2021-03-31 14:32:12 -0500

    fix infinite looping issue

--- my-app/src/components/GetEmployeeList.js
+++ my-app/src/components/GetEmployeeList.js
@@ -3,31 +3,38 @@ import API from '../utils/API'
 
 
 function GetEmployeeList() {
-    const [EmployeeInfo, setEmployeeInfo] = useState({
-        image: "",
-        firstName: "",
-        lastName: "",
-        address: "",
-        phone: "",
-        email: "",
-    })
+
+    const [EmployeeList, setEmployeeList] = useState("");
+
+    // const [EmployeeInfo, setEmployeeInfo] = useState({
+    //     image: "",
+    //     firstName: "",
+    //     lastName: "",
+    //     address: "",
+    //     phone: "",
+    //     email: "",
+    // })
 
     useEffect(() => {
         API.search()
             .then(res => {
-                console.log("employees retrieved: " + res)
-                setEmployeeInfo({
-                    image: res.data.results[0].picture.thumbnail,
-                    firstName: res.data.results[0].name.first,
-                    lastName: res.data.results[0].name.last,
-                    address: [res.data.results[0].location.street.number, res.data.results[0].location.street.name],
-                    phone: res.data.results[0].phone,
-                    email: res.data.results[0].email,
-                });
-                console.log(EmployeeInfo)
+                console.log("employees retrieved: " + res.data.results[0].picture.thumbnail);
+                let result = res.data.results
+                setEmployeeList({result})
+                console.log(EmployeeList)
+
+                // setEmployeeInfo({
+                    // image: res.data.results[0].picture.thumbnail,
+                    // firstName: res.data.results[0].name.first,
+                    // lastName: res.data.results[0].name.last,
+                    // address: [res.data.results[0].location.street.number, res.data.results[0].location.street.name],
+                    // phone: res.data.results[0].phone,
+                    // email: res.data.results[0].email,
             })
+                // console.log(EmployeeInfo)
+            // })
             .catch(err => console.log(err));
-    }, []);
+}, []);
 
     return ( 
         <div>
